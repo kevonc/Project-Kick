7b95bfe9ad0d762991dba040e8ab0fe3253888b6
Kevon Cheung <kevon.cheung@gmail.com>
2013-08-03 08:22:26 -0400

    Created repo, added development gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickstarter::Application.config.secret_token = '9dc67da3ae98f822f1869f6eaf72f85b6dc9ec338e5138695e3ff6e495e057eec1fa6f045dcf6afbf75f2d62db486d08fb25251c15d3c5ca46cd0680ff4c1ce8'
