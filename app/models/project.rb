a677f3baa44daec8afd37339466ca320876b9af4
Alina Guzman <alinochka8@gmail.com>
2013-08-04 14:05:30 -0400

    Added active record assocations to models and validations on the projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ActiveRecord::Base
+  belongs_to :city
+  belongs_to :category
   attr_accessible :backers, :category_id, :city_id, :funding, :goal, :title
+
+  validates :title, presence: true
+
+  validates :title, uniqueness: true
+
 end
