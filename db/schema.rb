cc8856a5655501dc133a7b461f5f76cda99ea546
Alina Guzman <alinochka8@gmail.com>
2013-08-04 15:21:10 -0400

    Added expired field to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130804175209) do
+ActiveRecord::Schema.define(:version => 20130804191637) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20130804175209) do
     t.integer  "category_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "expired"
   end
 
   create_table "users", :force => true do |t|
