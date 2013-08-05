c68ca28e642bba491acaf9736a521b589ab60d9c
Alina Guzman <alinochka8@gmail.com>
2013-08-05 17:43:02 -0400

    Changed project city request to use d3 enter selection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,17 +23,40 @@ $("#projectscities").click(function(){
         dataType: 'JSON'
     }).done(function(data){
             var svg = d3.select("svg");
-            var projection = d3.geo.albersUsa().scale(1224).translate([475,280]);
+            var dataEnter = svg.selectAll("circle")
+                .data(data).enter();
+            var projection = d3.geo.albersUsa()
+                .scale(1224)
+                .translate([475,280]);
 
-            for (i=0;i < data.length; i++){
-                var total_projects = data[i].total_projects;
-                var longitude = data[i].longitude;
-                var latitude = data[i].latitude;
-                svg.append("circle").attr("r",Math.log(total_projects)*8).attr("transform", function() {
-                    return "translate(" + projection([longitude, latitude]) + ")";
-                });
-            }
+            dataEnter.append("circle")
+                .attr("r", function(d, i)
+                { return Math.log(data[i].total_projects)*8 })
+                .attr("transform", function(d, i) {
+                    return "translate(" + projection([data[i].longitude, data[i].latitude]) + ")" });
+
+//            for (var i=0;i < data.length; i++){
+//                var total_projects = data[i].total_projects;
+//                var longitude = data[i].longitude;
+//                var latitude = data[i].latitude;
+//                svg.enter().append("circle")
+//                    .attr("r",Math.log(total_projects)*8)
+//                    .attr("transform", function() {
+//                    return "translate(" + projection([longitude, latitude]) + ")";
+//                });
+//            }
         });
 });
 });
+
+
+
+
+
+
+
+
+
+
+
 
