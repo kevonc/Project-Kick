f1edc518e7d5ab0dc1a09234e176358162022cfa
Alina Guzman <alinochka8@gmail.com>
2013-08-06 18:50:43 -0400

    Fixed appending issue with svg and circles

--- app/assets/javascripts/city.js
+++ app/assets/javascripts/city.js
@@ -53,7 +53,6 @@ function showfunding(){
         dataType: 'JSON'
     }).done(function(data){
             $("circle").remove();
-
             //setting d3 selection for enter
               dataEnter = svg.selectAll("circle")
                  .data(data);
@@ -73,40 +72,7 @@ function showfunding(){
                 .attr("r", function(d,i){
                     return Math.log(data[i].total_funding/50) })
                 .attr("fill", function(d,i){
-                        return "hsl(" + Math.log(data[i].total_funding)*30 + ",100%,50%)";
+                        return "hsl(" + Math.log(data[i].total_funding/50)*20 + ",100%,50%)";
                 });
         });
 }
-//hover events
-svg.selectAll("circle, text")
-    .on("mouseover", function(d) {
-        //Get this bar's x/y values, then augment for the tooltip
-        d3.select("#citydata")
-            .style("left", (d3.event.pageX) + 20 + "px")
-            .style("top", (d3.event.pageY) - 30 + "px")
-            .select("#value")
-            .text(d.school);
-//        d3.select('#seed-number')
-//            .text(d.seed);
-//        d3.select('#name')
-//            .text(d.school);
-//        d3.select('#expenses')
-//            .text("$" + addCommas(d.texpenses));
-//        d3.select('#revenue')
-//            .text("$" + addCommas(d.rev));
-//        d3.select('#appearance')
-//            .text(d.appearances);
-//        d3.select('#finalfours')
-//            .text(d.finalfours);
-//        d3.select('#titles')
-//            .text(d.titles);
-//        // for rounding values to million
-//        // .text("$" + parseInt(d.gross/1000000) + " million");
-//        d3.select("#tooltip").classed("hidden", false);
-    })
-
-    .on("mouseout", function() {
-
-        //Hide the tooltip
-        d3.select("#city").classed("hidden", true);
-    });
