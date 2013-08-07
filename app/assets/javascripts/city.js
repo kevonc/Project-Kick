c0e03d0c4ade5f9f907bd40a80d9dc10cfb7af28
Alina Guzman <alinochka8@gmail.com>
2013-08-07 11:38:34 -0400

    Styling the city data hover div

--- app/assets/javascripts/city.js
+++ app/assets/javascripts/city.js
@@ -34,14 +34,14 @@ function showprojects() {
             //appending circles to the map
             dataEnter.enter().append("circle")
                 .on("mouseover", function(d,i){
-                    d3.select("#tooltip")
+                    d3.select("#citydata")
                         .style("left", (d3.event.pageX) + 20 + "px")
                         .style("top", (d3.event.pageY) - 30 + "px");
                     d3.select("#city-name").text(data[i].name + '\n' + data[i].total_projects + " Projects");
-                    d3.select("#tooltip").classed("hidden", false);
+                    d3.select("#citydata").classed("hidden", false);
                 })
                 .on("mouseout", function() {
-                    d3.select("#tooltip").classed("hidden", true);
+                    d3.select("#citydata").classed("hidden", true);
                 })
                 .attr("r", 0)
                 .attr("transform", function(d, i) {
@@ -52,7 +52,7 @@ function showprojects() {
                     return Math.log(data[i].total_projects)*8
                 })
                 .attr("fill", function(d,i){
-                    return "hsla(" + Math.log(data[i].total_funding/50)*20 + ",100%,50%,.5)";
+                    return "hsla(" + Math.log(data[i].total_projects)*200 + ",100%,50%,.5)";
                 });
         });
 }
@@ -78,23 +78,23 @@ function showfunding(){
             //appending circles to the map
             dataEnter.enter().append("circle")
                 .on("mouseover", function(d,i){
-                    d3.select("#tooltip")
+                    d3.select("#citydata")
                         .style("left", (d3.event.pageX) + 20 + "px")
                         .style("top", (d3.event.pageY) - 30 + "px");
                     d3.select("#city-name").text(data[i].name + '\n $' + data[i].total_funding + " Funded");
-                    d3.select("#tooltip").classed("hidden", false);
+                    d3.select("#citydata").classed("hidden", false);
                 })
                 .on("mouseout", function() {
-                    d3.select("#tooltip").classed("hidden", true);
+                    d3.select("#citydata").classed("hidden", true);
                 })                .attr("r", 0)
                 .attr("transform", function(d, i) {
                     return "translate(" + projection([data[i].longitude, data[i].latitude]) + ")" })
                 .transition()
                 .duration(1000)
                 .attr("r", function(d,i){
-                    return Math.log(data[i].total_funding/5) })
+                    return Math.log(data[i].total_funding) })
                 .attr("fill", function(d,i){
-                        return "hsla(" + Math.log(data[i].total_funding/50)*20 + ",100%,50%,.5)";
+                        return "hsla(" + Math.log(data[i].total_funding)*55 + ",100%,50%,.5)";
                 });
         });
 }
