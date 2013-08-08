91d0ed531b132be6464dbdf170c400baf2954d64
Alina Guzman <alinochka8@gmail.com>
2013-08-07 20:10:46 -0400

    Began adding in page for showing funding by category

--- app/assets/javascripts/funding.js
+++ app/assets/javascripts/funding.js
@@ -1,21 +1,21 @@
-expired = Project.find_all_by_expired(true)
-expired.count
-ids = []
-expired.each do |project|
-    ids.push(project.category_id)
-end
-
-uniqueids = ids.uniq
-uniqueids.count
-
-thisidprojects = Project.find_all_by_category_id(237)
-overfunded = []
-thisidprojects.each do |project|
-    overfunded.push(project.overfunded)
-end
-
-
-
-
-
-# get all the projects from each category id
+//expired = Project.find_all_by_expired(true)
+//expired.count
+//ids = []
+//expired.each do |project|
+//    ids.push(project.category_id)
+//end
+//
+//uniqueids = ids.uniq
+//uniqueids.count
+//
+//thisidprojects = Project.find_all_by_category_id(237)
+//overfunded = []
+//thisidprojects.each do |project|
+//    overfunded.push(project.overfunded)
+//end
+//
+//
+//
+//
+//
+//# get all the projects from each category id
