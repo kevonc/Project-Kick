9a63b0ff3b6905df67113a26522eed14adacc749
Kevon Cheung <kevon.cheung@gmail.com>
2013-08-08 19:58:47 -0400

    Tweet page restyled

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,13 @@
 class TweetsController < ApplicationController
   def showtweets
-    @aggregated_tweets = []
-    @aggregated_tweets << Twitter.user_timeline("kickstarter", :count => 10)
-    @aggregated_tweets << Twitter.user_timeline("BestofKickstart", :count => 10)
-    @aggregated_tweets << Twitter.user_timeline("BestKickstarter", :count => 10)
-    @aggregated_tweets << Twitter.user_timeline("kickstartforum", :count => 10)
-    @aggregated_tweets << Twitter.user_timeline("KickstartPlanet", :count => 10)
-    @aggregated_tweets.flatten!.sort! { |a, b| b.created_at <=> a.created_at }
+    @official_tweets = []
+    @official_tweets << Twitter.user_timeline("kickstarter", :count => 10)
+    @official_tweets << Twitter.user_timeline("BestofKickstart", :count => 10)
+    @official_tweets << Twitter.user_timeline("BestKickstarter", :count => 10)
+    @official_tweets << Twitter.user_timeline("kickstartforum", :count => 10)
+    @official_tweets << Twitter.user_timeline("KickstartPlanet", :count => 10)
+    @official_tweets.flatten!.sort! { |a, b| b.created_at <=> a.created_at }
+
+    @people_tweets = Twitter.search("#kickstarter -rt", :lang => "en").attrs[:statuses]
   end
 end
