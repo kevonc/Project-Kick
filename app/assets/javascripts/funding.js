ee397ed4c578d79beeb07dd131cd90aa4ea51d28
Kevon Cheung <kevon.cheung@gmail.com>
2013-08-10 14:14:48 -0400

    Fixed funding js indentation issue

--- app/assets/javascripts/funding.js
+++ app/assets/javascripts/funding.js
@@ -1,100 +1,94 @@
 $(function(){
-    var w = 960,
-     h = 500,
+  var w = 960,
+   h = 420,
 
-    //Create SVG element
-     svg = d3.select("#overfunding")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+  //Create SVG element
+   svg = d3.select("#overfunding")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-$.ajax({
+  $.ajax({
     url: '/funding',
     type: 'GET',
     dataType: 'JSON'
-}).done(function(data){
+  }).done(function(data){
+    var dataset = data.sort(function(a,b) {
+       return parseFloat(b.percentage) - parseFloat(a.percentage);
+    });
 
-           var dataset = data.sort(function(a,b) {
-               return parseFloat(b.percentage) - parseFloat(a.percentage)
-           } );
+    var names = [],
+        values = [];
 
-        var names = [];
-        var values = [];
+    for (var i=0; i<8; i++){
+        names.push(dataset[i].name);
+        values.push(dataset[i].percentage);
+    }
 
-        for (var i=0; i<8; i++){
-            names.push(dataset[i].name);
-            values.push(dataset[i].percentage);
-        }
+    var xScale = d3.scale.ordinal()
+        .domain(d3.range(values.length))
+        .rangeRoundBands([0, w], .03);
 
-var xScale = d3.scale.ordinal()
-    .domain(d3.range(values.length))
-    .rangeRoundBands([0, w], .03);
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(dataset).percentage])
+        .range([0, h]);
 
-var yScale = d3.scale.linear()
-    .domain([0, d3.max(dataset).percentage])
-    .range([0, h]);
-
-        svg.selectAll("rect")
-            .data(values)
-            .enter()
-            .append("rect")
-            .attr("x", function(d, i) {
-                return xScale(i);
-            })
-            .attr("y", function(d) {
-                return h - yScale(d) + 50;
-            })
-            .attr("width", xScale.rangeBand())
-            .attr("height", function(d) {
-                return d ;
-            })
-            .attr("fill", function(d) {
-                return "#76CC1E";
-            })
-            .on("mouseover", function() {
-                d3.select(this)
-                    .attr("fill", "#FC6A6A");
-            })
-            .on("mouseout", function(d) {
-                d3.select(this)
-                    .transition()
-                    .duration(250)
-                    .attr("fill", "#76CC1E");
-            })
-          .on("click", function() {
-            sortBars();
-          });
-
+    svg.selectAll("rect")
+      .data(values)
+      .enter()
+      .append("rect")
+      .attr("x", function(d, i) {
+        return xScale(i);
+      })
+      .attr("y", function(d) {
+        return h - yScale(d) + 50;
+      })
+      .attr("width", xScale.rangeBand())
+      .attr("height", function(d) {
+        return d ;
+      })
+      .attr("fill", function(d) {
+        return "#76CC1E";
+      })
+      .on("mouseover", function() {
+        d3.select(this)
+         .attr("fill", "#FC6A6A");
+      })
+      .on("mouseout", function(d) {
+        d3.select(this)
+          .transition()
+          .duration(250)
+          .attr("fill", "#76CC1E");
+      })
+      .on("click", function() {
+        sortBars();
+      });
 
 //Create labels
-svg.selectAll("text")
-    .data(names)
-    .enter()
-    .append("text")
-    .text(function(d,i) {
+    svg.selectAll("text")
+      .data(names)
+      .enter()
+      .append("text")
+      .text(function(d,i) {
         return d + "  -  " + dataset[i].percentage + "%";
-    })
-    .attr("text-anchor", "middle")
-    .attr("x", function(d, i) {
-    //debugger;
+      })
+      .attr("text-anchor", "middle")
+      .attr("x", function(d, i) {
         return xScale(i) + xScale.rangeBand() / 2;
-    })
-    .attr("y", function(d,i) {
-       //return h - yScale(d);
-    //debugger;
-       //return 40 + (h-i);
-    return h - yScale(dataset[i].percentage)+50;
-    })
-  .attr("text-anchor", "middle")
-
-  .attr("font-family", "Tulpen One")
-    .attr("font-size", "25px")
-    .attr("fill", "black");
-    });
+      })
+      .attr("y", function(d,i) {
+        //return h - yScale(d);
+        //return 40 + (h-i);
+        return h - yScale(dataset[i].percentage)+50;
+      })
+      .attr("text-anchor", "middle")
+      .attr("font-family", "Tulpen One")
+      .attr("font-size", "25px")
+      .attr("fill", "black");
+  });
 });
 
 var sortBars = function() {
-
   svg.selectAll("rect")
     .sort(function(a, b) {
       return d3.descending(a, b);
@@ -104,7 +98,6 @@ var sortBars = function() {
     .attr("x", function(d, i) {
       return xScale(i);
     });
-
 };
 
 
