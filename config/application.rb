3d0f5f802551cfd5e42de1171eab1d8360810201
Kevon Cheung <kevon.cheung@gmail.com>
2013-09-30 18:04:57 +0800

    removed devise related config

--- config/application.rb
+++ config/application.rb
@@ -60,9 +60,6 @@ module Kickstarter
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    # heroku
-    config.assets.initialize_on_precompile = false
   end
 end
 
